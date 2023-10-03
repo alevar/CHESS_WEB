d7bd2ac17fcc2ad20a02053f1a80542caeffd04c
sparrow <alevar@gmail.com>
2023-10-02 22:27:27 -0400

    separate out db

--- CHESSApp_back/flaskconfig.py
+++ CHESSApp_back/flaskconfig.py
@@ -5,7 +5,7 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'data/chess.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '../CHESSApp_DB/data/chess.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 THREADS_PER_PAGE = 2
