f9fb58a2c9611b5d7416214d9890b43a415a507a
Sparrow <alevar@gmail.com>
2023-12-14 18:03:38 -0500

    sync

--- CHESSApp_front/src/App.tsx
+++ CHESSApp_front/src/App.tsx
@@ -1,17 +1,25 @@
 import logo from "./logo.svg"
-import { Counter } from "./components/Counter/Counter"
 import Home from "./components/Home/Home";
-import "./App.css"
-
+import Select from "./components/Select/Select";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import ContactUs from "./components/ContactUs/ContactUs";
+import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Home />
-      </header>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<ContactUs/>} />
+      <Route path="/select" element={<Select/>} />
+    </Routes>
     </div>
   )
 }
--- CHESSApp_front/src/components/Home/Home.tsx
+++ CHESSApp_front/src/components/Home/Home.tsx
@@ -1,68 +1,44 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-
-import {set_nascent, selectSettings} from "../../features/settings/settingsSlice"
-
-import './Home.css'
+import './Home.css';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <div className="container">
       <div className="home">
         <h1 className="header">Welcome to the CHESS Web Interface</h1>
         <p className="description">A unified resource for downloading existing and customizing genome annotations.</p>
         <p className="get-started">Get started by:</p>
-        <div className="row">
-          <div className="col-md-4">
-            <div className="card">
+        <div className="row d-flex">
+          <div className="col-md-4 d-flex">
+            <div className="card flex-fill" onClick={() => handleCardClick("/custom")}>
               <div className="card-body">
-                <Link to="/annotations" className="card-link">Customize Available Annotations</Link>
+                <Link to="/custom" className="card-link">Customize Available Annotations</Link>
               </div>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className="card">
+          <div className="col-md-4 d-flex">
+            <div className="card flex-fill" onClick={() => handleCardClick("/select")}>
               <div className="card-body">
-                <Link to="/annotations" className="card-link">Download Ready-To-Use CHESS 3 annotation</Link>
+                <Link to="/select" className="card-link">Download Ready-To-Use CHESS 3 annotation</Link>
               </div>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className="card">
+          <div className="col-md-4 d-flex">
+            <div className="card flex-fill" onClick={() => handleCardClick("/genes")}>
               <div className="card-body">
-                <Link to="/genes" className="card-link">Explore Individual genes</Link>
+                <Link to="/genes" className="card-link">Explore Individual Genes</Link>
               </div>
             </div>
           </div>
         </div>
-
-
-        <div>
-        {(() => {
-                  const settings = useSelector(selectSettings);
-                  const dispatch = useDispatch();
-
-                  const handleToggleNascent = (event: React.ChangeEvent<HTMLInputElement>) => {
-                    dispatch(set_nascent(event.target.checked));
-                  };
-
-                  return (
-                    <>
-                      <p>Current format: {settings.format}. Nascent included: {settings.include_nascent ? 'Yes' : 'No'}</p>
-                      <p>
-                        <input 
-                          type="checkbox" 
-                          checked={settings.include_nascent} 
-                          onChange={handleToggleNascent} 
-                        />
-                      </p>
-                    </>
-                  );
-                })()}
-        </div>
-
-
-
       </div>
     </div>
   );
