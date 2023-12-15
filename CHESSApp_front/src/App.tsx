d61c90b9115fcf4e41fc8c92e67cc1150ea97cf6
Alex Sparrow <alevar@gmail.com>
2023-12-15 15:30:35 -0500

    sync

--- CHESSApp_front/src/App.tsx
+++ CHESSApp_front/src/App.tsx
@@ -1,16 +1,30 @@
 import logo from "./logo.svg"
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import Home from "./components/Home/Home";
 import Select from "./components/Select/Select";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import ContactUs from "./components/ContactUs/ContactUs";
-import { useSelector, useDispatch } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { loadDBData } from "./features/database/databaseAPI";
 
 import "./App.css"
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    loadDBData().then(response => {
+      setData(response.data);
+    });
+  }, []); // Empty dependency array means this effect runs once on mount
+
   return (
     <div className="App">
     <Header />
