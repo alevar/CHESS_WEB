63a4cfd23e72a954cf8e3323729c6a552e055955
Alex Sparrow <alevar@gmail.com>
2023-12-18 21:36:00 -0500

    sync

--- CHESSApp_front/src/components/Custom/components/SourceSelection/SourceSelection.tsx
+++ CHESSApp_front/src/components/Custom/components/SourceSelection/SourceSelection.tsx
@@ -33,21 +33,31 @@ function SelectOrganism(props: Props) {
   const { sets, combinations } = useMemo(() => extractCombinations(elems), [elems]);
 
   const { selection, onSelectionChange, onPreviousSlide, prop_className } = props;
-  const [upsetSelection, setUpsetSelection] = React.useState(null);
-  const onElementClick = (element) => {
-    setUpsetSelection((prevSelection) => {
-      // Check if the element is already in the selection
-      if prevSelection === null {
-        return [element];
-      }
-      if (prevSelection.includes(element)) {
-        // If it is, remove it
-        return prevSelection.filter((e) => e !== element);
-      } else {
-        // If it's not, add it
-        return [...prevSelection, element];
-      }
-    });
+
+  const [selectedCombinations, setSelectedCombinations] = useState([]);
+  const handleSelectionChange = (combinations) => {
+    console.log("combinations",combinations);
+    // get previous selection
+    let newSelection = {...selectedCombinations};
+    console.log("newSelection",newSelection);
+    // combine combinations.elems with newSelection.elems (both are Arrays)
+    if (Object.keys(newSelection).length === 0) {
+      newSelection = {...combinations};
+    }
+    // add element if not present and remove if present
+    for (let elem of combinations.elems) {
+      // if (!newSelection.elems.includes(elem)) {
+      //   newSelection.elems.push(elem);
+      // } else {
+      //   newSelection.elems = newSelection.elems.filter((e) => e !== elem);
+      // }
+        console.log("elem",elem);
+        newSelection.elems.push(elem);
+    }
+    // also update newSelection.sets
+    newSelection.sets = { ...newSelection.sets, ...combinations.sets };
+    console.log("newSelection",newSelection);
+    setSelectedCombinations(newSelection);
   };
 
   const globalData = useSelector((state: RootState) => state.database.data);
@@ -102,11 +112,12 @@ function SelectOrganism(props: Props) {
         <UpSetJS
           sets={sets}
           combinations={combinations}
-          width={780}
-          height={400}
-          selection={upsetSelection}
-          onHover={setSelection}
-          onClick={onElementClick}
+          width={500}
+          height={300}
+          selection={selectedCombinations}
+          onClick={(newSelection) => {
+            handleSelectionChange(newSelection);
+          }}
         />
         </div>
       </div>
