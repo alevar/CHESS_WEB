d149af892bf3cd04262461889234e6be99d071c3
Alex Sparrow <alevar@gmail.com>
2023-12-19 15:16:55 -0500

    sync

--- CHESSApp_front/src/components/Custom/components/GeneSelection/GeneSelection.tsx
+++ CHESSApp_front/src/components/Custom/components/GeneSelection/GeneSelection.tsx
@@ -1,58 +1,32 @@
-import { useState, useContext } from 'react';
+import React, { useState, useMemo } from 'react';
+import { UpSetJS, extractCombinations, asCombinations } from '@upsetjs/react';
+import { createSetComposite } from '@upsetjs/plots/src/selections/single'
 
-interface Props {
-  selection: string;
-  onSelectionChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onNextSlide: () => void;
-  onPreviousSlide: () => void;
-  prop_className?: string;
-}
+function SelectOrganism() {
+  const [selection, setSelection] = useState(null);
 
-function SelectOrganism(props: Props) {
-  const { selection, onSelectionChange, onNextSlide, onPreviousSlide, prop_className } = props;
+  const handleSelectionChange = (set) => {
+    let new_set = createSetComposite([combinations[0],combinations[1]]);
+    setSelection(new_set);
+  }
 
-  const globalData = useContext(GlobalContext);
-
-  const isSelectionMade = selection !== '';
+  const elems = [
+      { name: 'A', sets: ['S1', 'S2'] },
+      { name: 'B', sets: ['S1'] },
+      { name: 'C', sets: ['S2'] },
+      { name: 'D', sets: ['S1', 'S3'] },
+    ];
+  const { sets, combinations } = useMemo(() => extractCombinations(elems), [elems]);
 
   return (
-    <div className={`${prop_className}`} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
-      <div className="row" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
-        <div className="col-md-6" style={{ borderRight: '1px solid #ccc', paddingRight: '15px' }}>
-          {Object.entries(globalData.organisms).map(([key, value], index) => (
-            <div className="form-check" key={index} style={{ display: 'flex', alignItems: 'center' }}>
-              <input
-                className="form-check-input"
-                type="radio"
-                name="selection1"
-                value={value.commonName}
-                checked={selection === value.commonName}
-                onChange={onSelectionChange}
-                style={{ marginRight: '10px' }}
-              />
-              <label className="form-check-label" style={{ marginRight: '20px' }}>
-                {value.commonName}
-              </label>
-            </div>
-          ))}
-        </div>
-        <div className="col-md-6 pl-md-5">
-          <p>Some general text goes here.</p>
-        </div>
-      </div>
-
-      <div style={{ position: 'absolute', bottom: '20px', width: '100%' }}>
-        <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-          <button className="btn btn-primary" onClick={onPreviousSlide}>
-            Previous
-          </button>
-          <button className="btn btn-primary" onClick={onNextSlide} disabled={!isSelectionMade}>
-            Next
-          </button>
-        </div>
-      </div>
-    </div>
+    <UpSetJS
+      sets={sets}
+      combinations={combinations}
+      width={500}
+      height={300}
+      selection={selection}
+      onClick={handleSelectionChange}
+    />
   );
 }
-
 export default SelectOrganism;
