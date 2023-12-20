213a0d9a5075a80578668d8f952fc2191870d1ed
Alex Sparrow <alevar@gmail.com>
2023-12-20 01:20:54 -0500

    sync

--- CHESSApp_front/src/components/Custom/components/SourceSelection/SourceSelection.tsx
+++ CHESSApp_front/src/components/Custom/components/SourceSelection/SourceSelection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
 
@@ -29,12 +29,115 @@ function SelectOrganism(props: Props) {
   const [buttonStates, setButtonStates] = useState({});
   const isSelectionMade = Object.values(buttonStates).some((value) => value === true);
 
+  // UpSetJS stuff
+  let upset_data = []
+  let selected_sources = []
+  for (const [key, value] of Object.entries(globalData?.upset[settings.value.species][settings.value.genome])) {
+    upset_data.push([key.split(';'),value]);
+    for (const name of key.split(';')) {
+      if (!selected_sources.includes(name)) {
+        selected_sources.push(name);
+      }
+    }
+  }
+
+  function areArraysEqual(array1: string[], array2: string[]): boolean {
+    return array1.length === array2.length && array1.every((value) => array2.includes(value));
+  }
+  
+  function processUpsetData(upset_data: UpsetData[], selected_sources: string[]): UpsetData[] {
+    const results: UpsetData[] = [];
+  
+    upset_data.forEach((data) => {
+      const commonNames = data[0].filter((name) => selected_sources.includes(name));
+  
+      if (commonNames.length > 0) {
+        const existingResult = results.find((result) =>
+          areArraysEqual(result[0], commonNames)
+        );
+  
+        if (existingResult) {
+          // If the combination exists, update the count
+          existingResult[1] += data[1];
+        } else {
+          // If the combination doesn't exist, add a new entry to results
+          results.push([
+            commonNames,
+            data[1],
+          ]);
+        }
+      }
+    });
+    
+    return results;
+  }
+
+  // now build elements from this data
+  function buildElements(data: UpsetData[]): any[] {
+    let elements: any[] = [];
+    let cardinalities = [];
+    let iname = 0;
+    data.forEach((data) => {
+      elements.push({ name: String(iname), sets: data[0] });
+      cardinalities.push(data[1]);
+      iname++;
+    });
+    return [elements,cardinalities];
+  }
+
+  const [sets, setSets] = useState([]);
+  const [combinations, setCombinations] = useState([]);
+
   const onButtonClick = (key) => {
     setButtonStates((prevStates) => {
-      const newState = prevStates[key] === true ? false : (prevStates[key] === false ? null : true);
+      const newState = !prevStates[key]; // Toggle the state
       return { ...prevStates, [key]: newState };
     });
   };
+
+  const onButtonClickUpdateUpset = (key) => {
+    // get all elements from buttonStates where value is true
+    let selected_sources = []
+    let found_key = false;
+    for (const [okey, value] of Object.entries(buttonStates)) {
+      if (key === okey) { 
+        found_key = true;
+        if (value === true) {// key was already set and is now being unset
+          continue;
+        }
+        else{
+          selected_sources.push(okey);
+        }
+      }
+      else{
+        if (value === true) {
+          selected_sources.push(okey);
+        }
+      }
+    }
+    if (!found_key){
+      selected_sources.push(key);
+    }
+
+    // update selected upset data
+    let subset_data = processUpsetData(upset_data, selected_sources);
+    console.log("subset",subset_data);
+    console.log(selected_sources);
+    // build elements
+    let [elems, combination_cardinalities] = buildElements(subset_data);
+
+    const { sets: newSets, combinations: newCombinations } = extractCombinations(elems);
+    for (let i=0; i<newCombinations.length; i++) {
+      newCombinations[i].cardinality = combination_cardinalities[i];
+    }
+    for (let i=0; i<newSets.length; i++) {
+      newSets[i].cardinality = globalData?.summary[settings.value.species][settings.value.genome][newSets[i].name]?.totalTranscripts;
+    }
+    setSets(newSets);
+    setCombinations(newCombinations);
+
+    console.log(combinations)
+  }
 
   const onNextSlide = () => {
     // Gather the selected checkboxes
@@ -42,17 +145,14 @@ function SelectOrganism(props: Props) {
       .filter(([key, value]) => value === true)
       .map(([key]) => key);
   
-    const exclude = Object.entries(buttonStates)
-      .filter(([key, value]) => value === false)
-      .map(([key]) => key);
-  
     // Dispatch the selected sources to the store
     dispatch(set_include_sources(include));
-    dispatch(set_exclude_sources(exclude));
   
     // Call the original onNextSlide callback
     props.onNextSlide();
   };
+  
+
 
   return (
     <div className={`${prop_className}`} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
@@ -63,8 +163,11 @@ function SelectOrganism(props: Props) {
           {Object.entries(globalData?.sources[settings.value.genome]).map(([key, value], index) => (
             <div key={index} className="col-sm-4" style={{ marginBottom: '10px' }}>
               <Button
-                variant={buttonStates[key] === true ? 'success' : (buttonStates[key] === false ? 'danger' : 'secondary')}
-                onClick={() => onButtonClick(key)}
+                variant={buttonStates[key] === true ? 'success' : 'secondary'}
+                onClick={() => {
+                  onButtonClick(key);
+                  onButtonClickUpdateUpset(key );
+                }}
                 style={{ marginRight: '10px', width: '100%', height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
               >
                 {key}
@@ -75,6 +178,12 @@ function SelectOrganism(props: Props) {
       </div>
         </div>
         <div className="col-md-6 pl-md-5">
+        <UpSetJS
+          sets={sets}
+          combinations={combinations}
+          width={600}
+          height={300}
+        />
         </div>
       </div>
 
