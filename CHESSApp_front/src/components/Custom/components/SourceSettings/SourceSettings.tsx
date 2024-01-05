a8787b4c436d30925e3542affb0a09e410e92bec
Alex Sparrow <alevar@gmail.com>
2024-01-05 14:45:12 -0500

    sync

--- CHESSApp_front/src/components/Custom/components/SourceSettings/SourceSettings.tsx
+++ CHESSApp_front/src/components/Custom/components/SourceSettings/SourceSettings.tsx
@@ -1,10 +1,147 @@
-const SourceSettings: React.FC = () => {
-    return (
-        <div className="custom-container">
-            <div className="custom-header">Source Settings</div>
-            {/* Add SourceSettings content here */}
-        </div>
-    );
-};
+import React, { useState, useMemo, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Accordion, Form, } from 'react-bootstrap';
 
-export default SourceSettings;
+import { DatabaseState } from '../../../../features/database/databaseSlice';
+import { SettingsState, set_include_sources, add_source, remove_source, add_attribute, remove_attribute } from '../../../../features/settings/settingsSlice';
+
+import { useGetTxSummarySliceQuery } from '../../../../features/database/databaseApi';
+
+interface RootState {
+  database: DatabaseState;
+  settings: SettingsState;
+}
+
+function SelectSources() {
+  
+    const globalData = useSelector((state: RootState) => state.database.data);
+    const settings = useSelector((state: RootState) => state.settings);
+    const dispatch = useDispatch();
+  
+    const [buttonStates, setButtonStates] = useState({});
+    const isSelectionMade = Object.values(buttonStates).some((value) => value === true);
+  
+    const [activeAccordionKey, setActiveAccordionKey] = useState(null);
+  
+    // Set initial button states - done to make sure all states are set
+    const initialButtonStatesSet = useRef(false);
+    useEffect(() => {
+      // Set initial button states only once when the component mounts
+      if (!initialButtonStatesSet.current) {
+        const initialButtonStates = {};
+        for (const sourceID of Object.values(settings.value.attributes)) {
+          for (const kvid of settings.value.attributes[sourceID]) {
+            initialButtonStates[kvid] = true;
+          }
+        }
+        setButtonStates(initialButtonStates);
+    
+        // Update the ref to prevent future runs of useEffect
+        initialButtonStatesSet.current = true;
+      }
+    }, [settings.value.attributes]);
+  
+    const onCheckboxChange = (sid, event) => {
+      // if checked, add to the list, otherwise remove
+      if (event.target.checked) {
+        dispatch(add_source(sid));
+      } else {
+        dispatch(remove_source(sid));
+      }
+  
+      // Prevent accordion from expanding when the switch is toggled
+      setActiveAccordionKey(null);
+    };
+  
+    const onButtonClick = (sourceID,kvid) => {
+      setButtonStates((prevStates) => {
+        const newState = !prevStates[kvid];
+        if (newState) {
+          dispatch(add_attribute([sourceID,kvid]));
+        } else {
+          dispatch(remove_attribute([sourceID,kvid]));
+        }
+        return { ...prevStates, [kvid]: newState };
+      });
+    };
+  
+    return (  
+        <div className="row" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+            <div className="container" style={{ height: '80vh', overflowY: 'auto' }}>
+            <div style={{ padding: '15px', fontSize: '1.5rem' }}>Source Settings</div>
+              <Accordion alwaysOpen activeKey={activeAccordionKey} onSelect={(key) => setActiveAccordionKey(key)} style={{ overflowY: 'auto' }}>
+                {globalData?.ass2src[settings.value.genome].map((sourceID, index) => {
+                  const isSourceIncluded = settings.value.sources_include.includes(Number(sourceID)) === true;
+  
+                  return (
+                    <Accordion.Item eventKey={sourceID.toString()} key={sourceID} className={'selected'}>
+                      <Accordion.Header style={
+                        isSourceIncluded
+                          ? {
+                              '--bs-accordion-active-bg': '#45b08c',
+                              '--bs-accordion-btn-bg': '#45b08c',
+                              display: 'flex',
+                              justifyContent: 'space-between',
+                              alignItems: 'center',
+                            }
+                          : { backgroundColor: 'inherit' }
+                      }>
+                        <span>{globalData?.sources[sourceID].name}</span>
+                        <div className="ml-auto">
+                          <Form.Check
+                            type="switch"
+                            id={`switch-${sourceID}`}
+                            checked={isSourceIncluded}
+                            onChange={(event) => onCheckboxChange(Number(sourceID), event)}
+                            label=""
+                            style={{ marginTop: 'auto', marginBottom: 'auto' }}
+                          />
+                        </div>
+                      </Accordion.Header>
+                      <Accordion.Body>
+                      {globalData?.src2gt.hasOwnProperty(Number(sourceID)) && 
+                        globalData?.src2gt[sourceID].length > 1 && (
+                        <>
+                          <h3>Gene Type</h3>
+                          <div>
+                            {globalData?.src2gt[sourceID].map((kvid, valueIndex) => (
+                              <Button
+                                key={kvid}
+                                variant={buttonStates[kvid] === true ? 'success' : 'secondary'}
+                                onClick={() => onButtonClick(sourceID, kvid)}
+                              >
+                                {globalData?.attributes[kvid].value}
+                              </Button>
+                            ))}
+                          </div>
+                        </>
+                      )}
+  
+                      {globalData?.src2tt.hasOwnProperty(Number(sourceID)) && 
+                        globalData?.src2tt[sourceID].length > 1 && (
+                        <>
+                          <h3>Transcript Type</h3>
+                          <div>
+                            {globalData?.src2tt[sourceID].map((kvid, valueIndex) => (
+                              <Button
+                                key={kvid}
+                                variant={buttonStates[kvid] === true ? 'success' : 'secondary'}
+                                onClick={() => onButtonClick(sourceID, kvid)}
+                              >
+                                {globalData?.attributes[kvid].value}
+                              </Button>
+                            ))}
+                          </div>
+                        </>
+                      )}
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  );
+                })}
+              </Accordion>
+            </div>
+          </div>
+    );
+  }
+  
+  export default SelectSources;
--- CHESSApp_front/src/components/Custom/components/SummaryView/SummaryView.tsx
+++ CHESSApp_front/src/components/Custom/components/SummaryView/SummaryView.tsx
@@ -1,33 +1,12 @@
-import UpsetPlot from './components/UpsetPlot/UpsetPlot';
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 
 const SummaryView: React.FC = () => {
-    const sampleData = {
-        sets: ['A', 'B', 'C', 'D'],
-        intersections: [
-            { set: 'A', value: 1000 },
-            { set: 'B', value: 500 },
-            { set: 'C', value: 800 },
-            { set: 'D', value: 300 },
-            { set: 'A,B', value: 200 },
-            { set: 'A,C', value: 400 },
-            { set: 'A,D', value: 100 },
-            { set: 'B,C', value: 300 },
-            { set: 'B,D', value: 200 },
-            { set: 'C,D', value: 100 },
-            { set: 'A,B,C', value: 100 },
-            { set: 'A,B,D', value: 100 },
-            { set: 'A,C,D', value: 0 },
-            { set: 'B,C,D', value: 100 },
-            { set: 'A,B,C,D', value: 0 },
-        ],
-    };
+  return (
+    <div>
 
-    return (
-        <div className="custom-container" id="test">
-            <div className="custom-header">Summary</div>
-            <UpsetPlot data={sampleData} />
-        </div>
-    );
+    </div>
+  );
 };
 
 export default SummaryView;
