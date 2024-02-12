074b66fd56f6286744f0ebaf7429443da4c9f31f
Alex Sparrow <alevar@gmail.com>
2024-02-12 12:34:29 -0500

    sync

--- CHESSApp_front/src/components/Main/components/Explore/ExploreGene/TxTable/makeData.ts
+++ CHESSApp_front/src/components/Main/components/Explore/ExploreGene/TxTable/makeData.ts
@@ -2,22 +2,8 @@ import { faker } from '@faker-js/faker'
 
 export type Person = {
     tid: string
-    lastName: string
-    age: number
-    visits: number
-    rand1: number
-    rand2: number
-    rand3: number
-    rand4: number
-    rand5: number
-    rand6: number
-    rand7: number
-    rand8: number
-    rand9: number
-    rand10: number
-    rand11: number
-    progress: number
-    status: 'relationship' | 'complicated' | 'single'
+    transcript: number
+    gene_id: number
     subRows?: Person[]
 }
 
@@ -32,26 +18,8 @@ const range = (len: number) => {
 const newPerson = (): Person => {
     return {
         tid: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        age: faker.number.int(40),
-        visits: faker.number.int(1000),
-        rand1: faker.number.int(100),
-        rand2: faker.number.int(100),
-        rand3: faker.number.int(100),
-        rand4: faker.number.int(100),
-        rand5: faker.number.int(100),
-        rand6: faker.number.int(100),
-        rand7: faker.number.int(100),
-        rand8: faker.number.int(100),
-        rand9: faker.number.int(100),
-        rand10: faker.number.int(100),
-        rand11: faker.number.int(100),
-        progress: faker.number.int(100),
-        status: faker.helpers.shuffle<Person['status']>([
-            'relationship',
-            'complicated',
-            'single',
-        ])[0]!,
+        transcript: faker.number.int(100),
+        gene_id: faker.number.int(3),
     }
 }
 
